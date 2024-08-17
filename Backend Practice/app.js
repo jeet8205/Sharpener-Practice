b63263c45d1702d16474e8ddbf3b2ce1b4929da9
Sayan Bandyopadhyay <85574454+jeet8205@users.noreply.github.com>
2024-08-17 12:23:55 +0530

    Parsed user request with bodyparser
    Took user input with an input box, parsed user request with body-parser, and stored data as object.

--- Backend Practice/app.js
+++ Backend Practice/app.js
@@ -1,15 +1,27 @@
 const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send(`
+        <form action ="/product" method ="POST">
+            <input type= "text" name = "title" placeholder="Product name">
+            <input type="text" name="size" placeholder ="Product size">
+            <button type="submit">Add Product</button>
+        </form>
+    `);
 });
 
-app.use((req, res, next) => {
-    console.log('in the second middleware');
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from express!!!</h1>');
 });
 
